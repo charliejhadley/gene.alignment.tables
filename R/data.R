ae81000d873a86bb26bc3936ec7d6fada71cb80f
martinjhnhadley <martinjohnhadley@gmail.com>
2017-12-11 11:07:14 +0000

    Placeholder documentation for package data

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' hbv_pol_sequence sequence data
+#'
+#' An example sequence for HBV Pol
+#'
+#' @format A data frame with 1,143 rows and 6 variables:
+#' \describe{
+#'   \item{position}{Gene position}
+#'   \item{reference}{Column to display}
+#'   \item{genom.a}{Alternative lettering}
+#'   \item{colour}{Colour to display this position as in the sequence viz}
+#' }
+#' @source \url{http://philippa-matthews.com}
+"hbv_pol_sequence"
+
+
+#' hbv_pol_sequence sequence data
+#'
+#' An example sequence for HBV Pol
+#'
+#' @format A data frame with 1,143 rows and 6 variables:
+#' \describe{
+#'   \item{position}{Gene position}
+#'   \item{reference}{Column to display}
+#'   \item{genom.a}{Alternative lettering}
+#'   \item{colour}{Colour to display this position as in the sequence viz}
+#' }
+#' @source \url{http://philippa-matthews.com}
+"hbv_pol_sequence"
+
+
