49792c393372f8fd2c4199b1a8129a75c24792e3
martinjhnhadley <martinjohnhadley@gmail.com>
2017-12-11 11:06:55 +0000

    Generate example data

---
+++ data-raw/data-clean.R
@@ -0,0 +1,38 @@
+library("tidyverse")
+
+hbv_pol_data <- read_csv("data-raw/hbv-pol-data.csv")
+
+sequence_region_colours <- list(
+  "Terminal protein" = "#ccfecc",
+  "Spacer" = "#f9fd74",
+  "Reverse transcriptase" = "#f4b084",
+  "RNAse H" = "#bdd7ee",
+  "Pre-S2" = "#fe6600",
+  "Surface antigen" = "#a9d08e",
+  NA = "#a6761d"
+)
+
+coding_region_colours <- as_tibble(sequence_region_colours) %>%
+  gather(coding.region, colour)
+
+save(coding_region_colours, file = "data/coding_region_colours.rdata")
+
+hbv_pol_sequence <- hbv_pol_data %>%
+  filter(sheet == "HBV Pol") %>%
+  select(-sheet, -african.data) %>%
+  mutate(colour = plyr::mapvalues(label, from = names(sequence_region_colours),
+                                  to = as.character(sequence_region_colours))) %>%
+  select(-label) %>%
+  replace_na(list(colour = "#a6761d"))
+
+save(hbv_pol_sequence, file = "data/hbv_pol_sequence.rdata")
+
+hbv_long_s_sequence <- hbv_pol_data %>%
+  filter(sheet == "HBV long S") %>%
+  select(-sheet, -african.data) %>%
+  mutate(colour = plyr::mapvalues(label, from = names(sequence_region_colours),
+                                  to = as.character(sequence_region_colours))) %>%
+  select(-label) %>%
+  replace_na(list(colour = "#a6761d"))
+
+save(hbv_long_s_sequence, file = "data/hbv_long_s_sequence.rdata")
