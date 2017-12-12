1fe02bdbbafbdb0a113ebe5d6a75d415baae8152
martinjhnhadley <martinjohnhadley@gmail.com>
2017-12-12 18:00:48 +0000

    Added runExample documentation

--- R/runExample.R
+++ R/runExample.R
@@ -1,8 +1,18 @@
+#' Run gene.alignment.table examples
+#'
+#' Launch a \code{gene.alignment.tables} example Shiny app using the built-in
+#' datasets. 
+#'
+#' @examples
+#' ## Only run this example in interactive R sessions
+#' if (interactive()) {
+#'   runExample()
+#' }
 #' @export
 runExample <- function() {
-  appDir <- system.file("shiny-examples", "myapp", package = "mypackage")
+  appDir <- system.file("shiny-examples", "hbv-alignment-viz", package = "gene.alignment.tables")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+    stop("Could not find example directory. Try re-installing `gene.alignment.tables`.", call. = FALSE)
   }
   
   shiny::runApp(appDir, display.mode = "normal")
