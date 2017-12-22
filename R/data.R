ed6349af2efe4fdd50e7a635ef1fc76663442492
martinjhnhadley <martinjohnhadley@gmail.com>
2017-12-22 10:31:51 +0000

    Added documentation for the coding_region_colours example dataset

--- R/data.R
+++ R/data.R
@@ -27,4 +27,14 @@
 #' @source \url{http://philippa-matthews.com}
 "hbv_long_s_sequence"
 
-
+#' coding_region_colours
+#'
+#' Example colour codings for different coding regions found in both example datasets, \code{hbv_long_s_sequence} and \code{hbv_pol_sequence}.
+#'
+#' @format A data frame with 6 rows and 2 variables:
+#' \describe{
+#'   \item{label}{Coding region name}
+#'   \item{colour}{Hex colour to use for the specified coding region}
+#' }
+#' @source \url{http://philippa-matthews.com}
+"coding_region_colours"
