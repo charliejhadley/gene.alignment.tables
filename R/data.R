4815f19c2be8a5043cdf53d7f94ce1f2d76dc60d
martinjhnhadley <martinjohnhadley@gmail.com>
2017-12-22 10:31:29 +0000

    Fixed documentation for the example sequence datasets

--- R/data.R
+++ R/data.R
@@ -1,30 +1,30 @@
 #' hbv_pol_sequence sequence data
 #'
-#' An example sequence for HBV Pol
+#' A fake genome sequence for example purposes.
 #'
-#' @format A data frame with 1,143 rows and 6 variables:
+#' @format A data frame with 843 rows and 4 variables:
 #' \describe{
 #'   \item{position}{Gene position}
 #'   \item{reference}{Column to display}
-#'   \item{genom.a}{Alternative lettering}
+#'   \item{genome.a}{Alternative lettering}
 #'   \item{colour}{Colour to display this position as in the sequence viz}
 #' }
 #' @source \url{http://philippa-matthews.com}
 "hbv_pol_sequence"
 
 
-#' hbv_pol_sequence sequence data
+#' hbv_long_s_sequence sequence data
 #'
-#' An example sequence for HBV Pol
+#' A fake genome sequence for example purposes.
 #'
-#' @format A data frame with 1,143 rows and 6 variables:
+#' @format A data frame with 300 rows and 4 variables:
 #' \describe{
 #'   \item{position}{Gene position}
 #'   \item{reference}{Column to display}
-#'   \item{genom.a}{Alternative lettering}
+#'   \item{genome.a}{Alternative lettering}
 #'   \item{colour}{Colour to display this position as in the sequence viz}
 #' }
 #' @source \url{http://philippa-matthews.com}
-"hbv_pol_sequence"
+"hbv_long_s_sequence"
 
 
